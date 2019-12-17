f81767f87958d9c646beebb57567323882ecf98c
ThomasBogdanov <ThomasBogdanov1@gmail.com>
2019-12-16 16:29:44 -0800

    Connected REST API

---
+++ server/src/datasources/launch.js
@@ -0,0 +1,42 @@
+const { RESTDatSource } = require("apollo-datasource-rest");
+
+class LaunchAPI extends RESTDataSource {
+  construbor() {
+    super();
+    this.baseURL = "https://api.spacexdata.com/v2/";
+	}
+	async getAllLaunches() {
+		const response = await this.get('launches');
+		return Array.isArray(response)
+			? response.map(launch => this.launchReducer(launch))
+			: [];
+	}
+	async getLaunchById({ launchId }) {
+		const response = await this.get('launches', { flight_number: launchId });
+		return this.launchReducer(response[0]);
+	}
+	launchReducer(launch) {
+		return {
+			id: launch.flight_number || 0,
+			cursor: `${launch.launch_date_unix}`,
+			site: launch.launch_site && launch.launch_site.site_name,
+			mission: {
+				name: launch.mission_name,
+				missionPatchSmall: launch.links.mission_patch_small,
+				missionPatchlarge: launch.links.mission_patch,
+			},
+			rocket: {
+				id: launch.rtocket.rocket_id,
+				name: launch.rocket.rocket_name,
+				type: launch.rocket.rocket_type,
+			},
+		};
+	}
+	getLaunchesByIds({ launchIds }) {
+		return Promise.all(
+			launchIds.map(launchID => this.getLaunchById({ launchId })),
+		);
+	}
+}
+
+module.exports = LaunchAPI;
